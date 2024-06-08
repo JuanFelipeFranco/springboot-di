fcdec903923b5f69c54d5a8e04247b22c674d489
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-08 12:42:27 -0500

    apply immutability using the cloneable interface

--- src/main/java/com/felipe/spring_di/app/springboot_di/services/ProductService.java
+++ src/main/java/com/felipe/spring_di/app/springboot_di/services/ProductService.java
@@ -12,8 +12,10 @@ public class ProductService {
     public List<Product> findAll() {
         return repository.findAll().stream().map(p -> {
             Double priceTax = p.getPrice() * 1.25d;
-            Product newProduct = new Product(p.getId(),p.getName(),priceTax.longValue())
+            //Product newProduct = new Product(p.getId(),p.getName(),priceTax.longValue())
 //            p.setPrice(priceTax.longValue());
+            Product newProduct = (Product) p.clone();
+            newProduct.setPrice(priceTax.longValue());
             return newProduct;
         }).collect(Collectors.toList());
     }
