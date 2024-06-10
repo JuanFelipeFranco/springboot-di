20d2f2eb257d9d2bc8831ca7dab964b987b0b94f
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-10 14:54:25 -0500

    Created Product Repository Interface e implement in Product Repository

--- src/main/java/com/felipe/spring_di/app/springboot_di/repositories/ProductRepository.java
+++ src/main/java/com/felipe/spring_di/app/springboot_di/repositories/ProductRepository.java
@@ -2,25 +2,10 @@ package com.felipe.spring_di.app.springboot_di.repositories;
 
 import com.felipe.spring_di.app.springboot_di.models.Product;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class ProductRepository {
-    private List<Product> data;
+public interface ProductRepository {
+    List<Product> findAll();
 
-    public ProductRepository() {
-        this.data = Arrays.asList(
-                new Product(1L, "Memoria corsair 32", 300L),
-                new Product(2L, "Cpu Intel Core i9", 850L),
-                new Product(3L, "Teclado Razer Mini 60%", 180L),
-                new Product(4L, "Motherboard Gigabyte", 490L));
-    }
-
-
-    public List<Product> findAll() {
-        return data;
-    }
-    public Product findById(Long id) {
-        return data.stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
-    }
+    Product findById(Long id);
 }
