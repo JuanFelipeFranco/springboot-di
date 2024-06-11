be8bc1a1ec53c0d85b90d95cc1f3b384fb3d261f
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-11 13:47:48 -0500

    Implementing config.properties in Service Class
    Implementando taxes a traves de properties.

--- src/main/java/com/felipe/spring_di/app/springboot_di/services/ProductServiceImpl.java
+++ src/main/java/com/felipe/spring_di/app/springboot_di/services/ProductServiceImpl.java
@@ -4,6 +4,8 @@ import com.felipe.spring_di.app.springboot_di.models.Product;
 import com.felipe.spring_di.app.springboot_di.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +13,12 @@ import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${config.price.tax}")
+    private Double tax;
     @Autowired
     @Qualifier("productList")
     private ProductRepository repository;
@@ -22,15 +30,17 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public List<Product> findAll() {
         return repository.findAll().stream().map(p -> {
-            Double priceTax = p.getPrice() * 1.25d;
+//            Double priceTax = p.getPrice() * environment.getProperty("config.price.tax", Double.class);
+            Double priceTax = p.getPrice() * tax;
             //Product newProduct = new Product(p.getId(),p.getName(),priceTax.longValue())
 //            p.setPrice(priceTax.longValue());
+
             Product newProduct = (Product) p.clone();
             newProduct.setPrice(priceTax.longValue());
             return newProduct;
+
 //            p.setPrice(priceTax.longValue());
 //            return p;
-
         }).collect(Collectors.toList());
     }
 
