4aa5c34249f491b9520ab21365c78820b157f272
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-11 12:44:35 -0500

    Change Clone to @RequestScope

--- src/main/java/com/felipe/spring_di/app/springboot_di/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/felipe/spring_di/app/springboot_di/repositories/ProductRepositoryImpl.java
@@ -3,11 +3,13 @@ package com.felipe.spring_di.app.springboot_di.repositories;
 import com.felipe.spring_di.app.springboot_di.models.Product;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.Arrays;
 import java.util.List;
 
-//@Primary
+@Primary
+@RequestScope
 @Repository("productList")
 public class ProductRepositoryImpl implements ProductRepository {
     private List<Product> data;
