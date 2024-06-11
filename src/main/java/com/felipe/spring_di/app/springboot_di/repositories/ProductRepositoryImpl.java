45116e9cce2665ce3f2523bea21a9ec3e3700ce5
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-11 13:19:30 -0500

    @RequestScope @SessionScope Singleton Different Context

--- src/main/java/com/felipe/spring_di/app/springboot_di/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/felipe/spring_di/app/springboot_di/repositories/ProductRepositoryImpl.java
@@ -3,13 +3,17 @@ package com.felipe.spring_di.app.springboot_di.repositories;
 import com.felipe.spring_di.app.springboot_di.models.Product;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.context.annotation.RequestScope;
+
+//import org.springframework.web.context.annotation.RequestScope;
+//import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.Arrays;
 import java.util.List;
 
+
+//@RequestScope
+//@SessionScope
 @Primary
-@RequestScope
 @Repository("productList")
 public class ProductRepositoryImpl implements ProductRepository {
     private List<Product> data;
